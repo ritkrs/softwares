c5528a61e278d26228c04f2777a13a4d36564769
“Ritesh <ritkrs91@gmail.com>
2022-01-18 15:32:14 +0530

    softwares

---
+++ src/main/java/com/ecom/dao/ProductCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.ecom.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.ecom.entity.ProductCategory;
+@RepositoryRestResource(collectionResourceRel = "productCategory", path="product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+	
+
+}
